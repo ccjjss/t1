f2b25c6c7ad3fba8f7b446d30bb608b939f33a67
ccjjss <ccjjss@hotmail.com>
2024-08-29 17:20:33 -0500

    fix version python deploy.yaml V30

--- function/main.py
+++ function/main.py
@@ -3,8 +3,8 @@
 from google.cloud import bigquery
 
 def get_data(request):
-    client = bigquery.Client()
-    query = "SELECT * FROM `latam-project.latam_analytics_dataset.latam_analytics_table` LIMIT 10"
+    client = bigquery.Client(project="berrysafetest-15c25")
+    query = "SELECT * FROM `latam_analytics_dataset.latam_analytics_table` LIMIT 10"
     query_job = client.query(query)
     results = query_job.result()
     rows = [dict(row) for row in results]
