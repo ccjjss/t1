3d8393959a3e888d517cfc59656a0f040e1bccd2
ccjjss <ccjjss@hotmail.com>
2024-08-31 16:49:17 -0500

    fix version python deploy.yaml V88

--- terraform/modules/cloud_function_pubsub/main.tf
+++ terraform/modules/cloud_function_pubsub/main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "process-pubsub-bucket" {
+resource "google_storage_bucket" "process_pubsub_bucket" {
   name          = var.bucket_name
   location      = var.location
   force_destroy = true
